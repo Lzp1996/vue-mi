58b04a8678cf323b02cacdfa0afe3f46f587f892
li771547 <771547353@qq.com>
2021-02-12 17:42:35 +0800

    init-project

---
+++ src/components/NavFooter.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>navfooter</div>
+</template>
+
+<script>
+export default {
+  name: 'NavFooter'
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,31 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
+axios.defaults.baseURL = '/api'
+axios.defaults.timeout = 8000
+
+axios.interceptors.response.use(function (response) {
+  const res = response.data
+  const path = location.hash
+  if (res.status === 0) {
+    return res.data
+  } else if (res.status === 10) {
+    if (path !== '#/index') {
+      window.location.href = '/#/login'
+    }
+    return Promise.reject(res)
+  } else {
+    return Promise.reject(res)
+  }
+}, error => {
+  // const res = error.response
+  return Promise.reject(error)
+})
+
+Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
 
 new Vue({
